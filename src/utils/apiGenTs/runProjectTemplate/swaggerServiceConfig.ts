c741557cc0670424e105cb701fd5aabab9d9d11e
laotianwy <1695657342@qq.com>
2025-01-06 21:45:09 +0800

    feat：添加接口baseURL文件，供调用方使用

---
+++ src/utils/apiGenTs/runProjectTemplate/swaggerServiceConfig.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: laotianwy 1695657342@qq.com
+ * @Date: 2025-01-06 18:33:58
+ * @LastEditors: laotianwy 1695657342@qq.com
+ * @LastEditTime: 2025-01-06 18:34:13
+ * @FilePath: /cli/src/utils/apiGenTs/runProjectTemplate/swaggerServiceConfig.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+export const serviceConfig = (() => {
+    return {
+        // 所有接口的baseUrl地址
+        baseURL: 'https://www.baidu.com/api'
+    };
+})();
+
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-04 18:58:49
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-06 03:17:08
+ * @LastEditTime: 2025-01-06 18:35:33
  * @FilePath: /npm/cli/tsup.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,6 +19,7 @@ export default defineConfig({
     clean: true,
     onSuccess: async () => {
         await copy('src/utils/apiGenTs/templatesDir/', 'dist/templatesDir');
+        await copy('src/utils/apiGenTs/runProjectTemplate/','dist/runProjectTemplate')
         console.log('build after copy success')
     }
 })
