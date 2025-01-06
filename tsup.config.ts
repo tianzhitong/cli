68babce8b761cbf86a4b4b7bc9713f23b062edbe
laotianwy <1695657342@qq.com>
2025-01-07 00:49:17 +0800

    build：修改版本号为1.0.1

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-04 18:58:49
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-06 18:35:33
+ * @LastEditTime: 2025-01-07 00:47:30
  * @FilePath: /npm/cli/tsup.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,10 +12,10 @@ import { copy } from 'fs-extra';
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ['esm'],
-    minify: false,
+    minify: true,
     dts: false,
     splitting: false,
-    sourcemap: true,
+    sourcemap: false,
     clean: true,
     onSuccess: async () => {
         await copy('src/utils/apiGenTs/templatesDir/', 'dist/templatesDir');
