f43e94daca6d3dd0a7355e9b4aa2a7795466ecea
laotianwy <1695657342@qq.com>
2025-01-07 21:45:56 +0800

    docs：文档修改

--- apiGenTs.config.js
+++ apiGenTs.config.js
@@ -2,11 +2,12 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-06 04:04:39
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-06 04:06:28
+ * @LastEditTime: 2025-01-07 21:40:07
  * @FilePath: /cli/apiGenTs.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
+/** @type {import("tianzhitong-cli/dist").GlobalConfigProps} */
 const config = {
     swaggerList: [
         {
--- apiGenTs.ts
+++ apiGenTs.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-05 22:11:15
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-07 20:56:19
+ * @LastEditTime: 2025-01-07 21:43:28
  * @FilePath: /cli/apiGenTs.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,11 +23,12 @@ export interface swaggerListProps {
     name: string;
     /** 当前生成的接口的前缀地址 /api 开头 */
     baseUrl?: string;
-    /** 如果穿入当前属性。只生成部分接口。 */
+    /** 如果传入当前属性。只生成部分接口。 */
     apis: Partial<apiProps>;
 };
 
 export interface configProps {
+    /** swagger接口数据open 数据 */
     swaggerList: Array<swaggerListProps>,
 }
 
