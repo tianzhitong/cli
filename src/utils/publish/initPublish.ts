3c4070991f08b4ab6c880909a80eca36dadf531d
laotianwy <1695657342@qq.com>
2025-01-08 21:21:48 +0800

    feat：publish功能修改。如果没找到该包，那么不修改版本号

--- src/utils/publish/initPublish.ts
+++ src/utils/publish/initPublish.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-04 23:59:19
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-05 17:57:16
+ * @LastEditTime: 2025-01-08 21:18:13
  * @FilePath: /cli/src/utils/initPublish.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,13 +21,15 @@ export interface initPublishProps {
 const initPublish = async (props: initPublishProps) => {
     try {
         // 【1】获取当前要发布包的版本
-        const version = await getPackageVersion();
-        // 【2】修改版本号
-        const lastUseVersion = await editPwdVersion(props, version);
+        const { version, npmExist } = await getPackageVersion();
+
+        // 【2】修改之后的版本号
+        const editAfterVersion = npmExist ? await editPwdVersion(props, version) : version;
+
         // 【3】发布npm
         await npmPublish();
         // 【4】git添加tag信息
-        await gitAddVersionTag(lastUseVersion);
+        await gitAddVersionTag(editAfterVersion);
         // 退出程序
         shelljs.exit(1);
     } catch (ex) {
