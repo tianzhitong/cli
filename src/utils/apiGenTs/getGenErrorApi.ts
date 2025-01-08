0417738cbd8dd4dc61aab9af7128e2bd9434e159
laotianwy <1695657342@qq.com>
2025-01-09 00:56:24 +0800

    feat：接口构建失败添加提示

---
+++ src/utils/apiGenTs/getGenErrorApi.ts
@@ -0,0 +1,83 @@
+/*
+ * @Author: laotianwy 1695657342@qq.com
+ * @Date: 2025-01-08 23:47:07
+ * @LastEditors: laotianwy 1695657342@qq.com
+ * @LastEditTime: 2025-01-09 00:52:40
+ * @FilePath: /cli/src/utils/apiGenTs/getGenErrorApi.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+import chalk from "chalk";
+import logSymbols from "../common/logSymbols";
+import { resolveApp } from "../common/removeDir";
+import { pathToFileURL } from "node:url";
+import pkg from "fs-extra";
+
+const { writeJsonSync, removeSync } = pkg;
+
+const getGenErrorApi = (remoteSwaggerDataList: Map<any, any>) => {
+    const noGenerateApi = new Map();
+    const keys = Array.from(remoteSwaggerDataList.keys());
+    keys.forEach(swaggerSingInfo => {
+        const getSwaggerSpecData = remoteSwaggerDataList.get(swaggerSingInfo);
+        noGenerateApi.set(swaggerSingInfo.name, [])
+        // 获取构建失败的接口
+        const pathMap = Reflect.ownKeys(getSwaggerSpecData?.paths ?? {});
+        const getRemoteApiInfo = new Map();
+        if (pathMap.length > 0) {
+            pathMap.forEach(currentApi => {
+                // 获取到远程接口的所有方法。
+                const currentApiRequestMethods = Reflect.ownKeys(getSwaggerSpecData.paths[currentApi]).map(item => String(item).toUpperCase());
+                getRemoteApiInfo.set(currentApi, currentApiRequestMethods);
+            })
+        }
+        const currentApiNoFind = noGenerateApi.get(swaggerSingInfo.name);
+        Reflect.ownKeys(swaggerSingInfo?.apis ?? {}).forEach(requestMethod => {
+            (swaggerSingInfo.apis[requestMethod] ?? []).forEach(requestUrl => {
+                const remoteRequestApiCurrentMethod = getRemoteApiInfo.get(requestUrl);
+                if (!remoteRequestApiCurrentMethod) {
+                    // 如果不存在，那么就是没找到当前接口
+                    currentApiNoFind.push({
+                        method: requestMethod,
+                        url: requestUrl
+                    })
+                } else {
+                    if (!(remoteRequestApiCurrentMethod as any[]).includes(requestMethod)) {
+                        // 包含了该接口
+                        currentApiNoFind.push({
+                            method: requestMethod,
+                            url: requestUrl
+                        })
+                    }
+                }
+            })
+        })
+    });
+
+    // 获取未找到的接口的数量
+    let noFindApiNum = 0;
+    const getNoGenerateApiKeys = Array.from(noGenerateApi.keys());
+    getNoGenerateApiKeys.forEach(item => {
+        noFindApiNum += noGenerateApi.get(item).length
+    })
+
+    const genErrorPath = resolveApp('./apiGenTs.error.json');
+
+    if (noFindApiNum > 0) {
+        const genNoUseApiJSON = {};
+        getNoGenerateApiKeys.forEach(key => {
+            genNoUseApiJSON[key] = noGenerateApi.get(key)
+        });
+        writeJsonSync(genErrorPath, genNoUseApiJSON,{
+            spaces: 4,
+        })
+        console.log(logSymbols.error, chalk.yellow(`部分接口构建失败。请查看 ——-->${pathToFileURL(genErrorPath)}`));
+
+        return;
+    }
+
+    removeSync(genErrorPath)
+
+}
+
+export default getGenErrorApi;
--- src/utils/apiGenTs/index.ts
+++ src/utils/apiGenTs/index.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-05 22:05:48
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-08 01:22:40
+ * @LastEditTime: 2025-01-09 00:25:01
  * @FilePath: /cli/src/utils/apiGenTs/index.ts
  * @Description: 根据配置文件生成ts文件
  */
@@ -19,6 +19,7 @@ import filterNoUseApi from "./filterNoUseApi";
 import { API_CONFIG_BASE_URL_FILE, API_GEN_TS_THROW_DIR_NAME } from "../../config/const";
 import createManyServiceFileBySwaggler from './createManyServiceFileBySwaggler'
 import initConfigFileToProject from "./createFileToProject";
+import getGenErrorApi from "./getGenErrorApi";
 
 const apiGenTs = async () => {
     try {
@@ -34,9 +35,12 @@ const apiGenTs = async () => {
         if ((getConfigFileInfo?.swaggerList ?? []).length === 0) {
             throw new Error(`${logSymbols.error}${chalk.yellow('获取swaggerList属性失败！')}`);
         }
+
+        const remoteSwaggerDataList = new Map();
         // 【2】根据配置文件来生成ts文件
         for (let index = 0; index < getConfigFileInfo.swaggerList.length; index++) {
             const swaggerSingInfo = getConfigFileInfo.swaggerList[index];
+
             // 远程api接口的配置文件
             let getSwaggerSpecData: any;
             if (swaggerSingInfo.url.includes('http')) {
@@ -44,6 +48,9 @@ const apiGenTs = async () => {
             } else {
                 getSwaggerSpecData = fs.readJsonSync(resolveApp(swaggerSingInfo.url));
             }
+
+            remoteSwaggerDataList.set(swaggerSingInfo,getSwaggerSpecData)
+
 
             // 过滤不需要的api
             const getNewSpecToSwagger = filterNoUseApi({
@@ -66,10 +73,13 @@ const apiGenTs = async () => {
             })
         }
 
+        // 查询config的接口是否被创建成功
+        getGenErrorApi(remoteSwaggerDataList)
+
         // 动态生成api文件事其他文件调用
-        createManyServiceFileBySwaggler(getConfigFileInfo.swaggerList,outApiDirPath);
+        createManyServiceFileBySwaggler(getConfigFileInfo.swaggerList, outApiDirPath);
         // 创建配置文件到项目里去
-        await initConfigFileToProject(resolve(outApiDirPath,`../${API_CONFIG_BASE_URL_FILE}`));
+        await initConfigFileToProject(resolve(outApiDirPath, `../${API_CONFIG_BASE_URL_FILE}`));
         console.log(logSymbols.success, chalk.green(`动态生成ts文件成功！`));
     } catch (ex) {
         console.log('ex', ex)
