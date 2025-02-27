8c636618287d3326c8aaed6559146f37bf3a45df
laotianwy <1695657342@qq.com>
2025-02-28 00:02:13 +0800

    fix：修复错误

--- src/utils/apiGenTs/createRemoteMockApi.ts
+++ src/utils/apiGenTs/createRemoteMockApi.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-24 15:33:02
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-27 23:59:13
+ * @LastEditTime: 2025-02-28 00:01:23
  * @FilePath: /cli/src/utils/apiGenTs/createRemoteMockApi.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -97,7 +97,7 @@ export const createRemoteMockApi = async (
                         responseToTockStructData: JSON.stringify(responseToTockStructData),
                     };
                     pushServerData.push(mockServeData);
-                });
+                }
             });
 
             sendApiData.push(...pushServerData);
